4b719e423a11da25566834647fe2277293d7ac31
Задорин Артем <artfan95@gmail.com>
2024-05-25 22:25:55 +0700

    Настрйока webpack для работы на ract

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
+const { watchFile } = require('fs');
+const FilemanagerWebpackPlugin = require('filemanager-webpack-plugin');
+const HmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/main.js',
+    entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'app/js'),
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'app/'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        new HmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html'
+
+        }),
+        new FilemanagerWebpackPlugin({
+            events: {
+                onStart: {
+                    delete: ['app']
+                }
+            }
+        })
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'app')
+    }
 };
